3d2f3df8c3b4cd8ae21854870dd5c906204bf326
Hannah <hannahjama@Hannahs-MacBook-Pro.local>
2025-04-30 16:10:22 +0100

    fixing vercel deploy bug

---
+++ src/app/studio/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Studio Tool',
+  description: 'Tool view for studio',
+};
+
+export default function StudioLayout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
